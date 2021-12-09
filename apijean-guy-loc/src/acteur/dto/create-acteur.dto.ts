4e96a8d900e95e330354c249f24705f9a9ea569a
Romain29000 <44052164+Romain29000@users.noreply.github.com>
2021-12-09 16:55:46 +0100

    fix : Acteur / Realisateur Validation (IsNotEmpty)

--- apijean-guy-loc/src/acteur/dto/create-acteur.dto.ts
+++ apijean-guy-loc/src/acteur/dto/create-acteur.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateActeurDto {
+    @IsNotEmpty()
     nom : string;
+
+    @IsNotEmpty()
     prenom : string;
 }
--- apijean-guy-loc/src/acteur/dto/update-acteur.dto.ts
+++ apijean-guy-loc/src/acteur/dto/update-acteur.dto.ts
@@ -1,7 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateActeurDto } from './create-acteur.dto';
+import { IsNotEmpty } from "class-validator";
 
 export class UpdateActeurDto extends PartialType(CreateActeurDto) {
+    @IsNotEmpty()
     nom : string;
+
+    @IsNotEmpty()
     prenom : string;
 }
