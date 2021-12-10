06c01281c20a5d8b1553ee3467d9f8ddde65e1fe
Romain29000 <44052164+Romain29000@users.noreply.github.com>
2021-12-10 09:14:30 +0100

    Throw errors for note format

--- apijean-guy-loc/src/note/note.service.ts
+++ apijean-guy-loc/src/note/note.service.ts
@@ -13,9 +13,25 @@ export class NoteService {
 
 
   async create(createNoteDto: CreateNoteDto) {
+    if(createNoteDto.valeur > 5 || createNoteDto.valeur < 0) {
+      throw new HttpException({
+        status: HttpStatus.CONFLICT,
+        error: 'Note must be between 0 and 5',
+      }, HttpStatus.CONFLICT);
+    }
+
+    let valTest = createNoteDto.valeur * 100;
+    let modulo = 0.5 * 100;
+    if(((valTest % modulo) / 100) != 0) {
+      throw new HttpException({
+        status: HttpStatus.CONFLICT,
+        error: 'Incorrect note format',
+      }, HttpStatus.CONFLICT);
+    }
+
     const note = await this.noteRepo.create(createNoteDto);
     await this.noteRepo.save(note);
-    return note;  
+    return note;
   }
 
   async findAll() {
